1914e47914975396ff94b072bdf253e977dece0c
xdCao <705083979@qq.com>
2017-12-19 11:21:06 +0800

    start

---
+++ src/main/java/xd/medicine/MedicineApplication.java
@@ -0,0 +1,12 @@
+package xd.medicine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicineApplication.class, args);
+	}
+}
---
+++ src/test/java/xd/medicine/MedicineApplicationTests.java
@@ -0,0 +1,16 @@
+package xd.medicine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MedicineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
