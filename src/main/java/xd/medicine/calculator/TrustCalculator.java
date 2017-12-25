8b2543faac8cb894516e7d524a8bf6fcfa075fab
liubotao527 <1274830046@qq.com>
2017-12-25 20:49:43 +0800

    sql里插入了一些测试数据

--- src/main/java/xd/medicine/calculator/TrustCalculator.java
+++ src/main/java/xd/medicine/calculator/TrustCalculator.java
@@ -5,6 +5,7 @@ import xd.medicine.entity.bo.Doctor;
 import xd.medicine.entity.bo.Patient;
 import xd.medicine.entity.bo.SysLog;
 import xd.medicine.entity.bo.UserLog;
+import xd.medicine.entity.dto.DoctorTrustResult;
 import xd.medicine.entity.dto.PatientWithTrust;
 import xd.medicine.service.CommentService;
 import xd.medicine.service.DoctorService;
@@ -36,26 +37,32 @@ public class TrustCalculator {
     输入一个病人id，输出匹配到的主体集及其匹配可信度、历史行为可信度、总体可信度，暂时仅打印出来
      */
     public void calTrust( int patientId ) {
-        float mt,hbt,trust;
+        float mt,hbt,rcm,rep,trust;
         PatientWithTrust patient = patientService.getPatientById(patientId);
         List<Doctor> doctorList = patientService.getSisDoctorsByPatientId(patientId);  //获得满足科室要求的所有医生，即候选主体集合SIS
         for (Doctor doctor : doctorList) {
-            System.out.print(doctor.getId() + "---");
-            System.out.print(doctor.getName() + "---");
+
             if (patient.getPatient().getDoctorId() == doctor.getId()) {
                 /* 如果是主治医生，所有可信度直接为1 */
                 mt = 1;
                 hbt = 1;
                 trust = 1;
+                rcm = 1;
+                rep = 1;
             } else {
                 mt = calDocMt(doctor);
-                hbt = calDocHbt(doctor);
+                rcm = calDocRCM(doctor);
+                rep = calDocREP(doctor);
+                if(rcm<0||rcm>1||rep<0||rep>1){
+                    System.out.println("ERROR RCM OR REP!医生id："+ doctor.getId()+"医生姓名："+doctor.getName());
+                    continue;
+                }
+                hbt = rcm<rep?rcm:rep;     //HBT = min(RCM,REP)
                 trust = mt*TRUSTU + hbt*(1-TRUSTU);
             }
-            System.out.print("MT:" + mt + "--");
-            System.out.println("HBT:"+hbt+"--");
-            System.out.println("TRUST:"+trust+"--");
-            System.out.println("free?:" + (doctor.getIsFree() ? "yes" : "no"));
+
+            DoctorTrustResult doctorTrustResult = new DoctorTrustResult(doctor, mt, rcm, rep, hbt, trust);
+            System.out.println(doctorTrustResult.toString());
         }
     }
 
@@ -88,7 +95,7 @@ public class TrustCalculator {
     public float calDocRCM(Doctor doctor){
         List<UserLog> userLogList = commentService.getAllUserLogsByDoctor(doctor.getId());
         if(userLogList.size()< HBTN){
-            System.out.println("ERROR！！样本数量不足！！!");
+            System.out.println("ERROR！！用户日志样本数量不足！！!医生id："+ doctor.getId()+"医生姓名："+doctor.getName());
             return -1;
         }
 
@@ -96,8 +103,8 @@ public class TrustCalculator {
         for(int i=0; i<HBTM; i++){
             int sum = 0;
             List<Integer> list = sampling(userLogList.size(),HBTN); //进行抽样
-            for(int j=0 ; j< HBTN; j++){
-                sum += (userLogList.get(list.get(j)).getEvaluateValue())&0xFF;
+            for(int j=0 ; j < HBTN; j++){
+                sum += (userLogList.get(list.get(j)-1).getEvaluateValue())&0xFF;
             }
             samplingAveValue.add((float)sum/HBTN/100); //评价值为0-100，计算时以1为最大值进行量化
         }
@@ -120,6 +127,10 @@ public class TrustCalculator {
     */
     public float calDocREP(Doctor doctor){
         List<SysLog> sysLogList = commentService.getAllSysLogsByDoctor(doctor.getId());
+        if(sysLogList.size()==0){
+            System.out.println("ERROR！！系统日志样本数量不足！！!医生id："+ doctor.getId()+"医生姓名："+doctor.getName());
+            return -1;
+        }
         List<Integer> list = new ArrayList<>(sysLogList.size());
         for(SysLog sysLog : sysLogList){
             list.add(sysLog.getEvaluateValue()&0xFF);
@@ -130,7 +141,7 @@ public class TrustCalculator {
     /*
     *给定一个医生，计算其HBT
      */
-    public float calDocHbt(Doctor doctor){
+    /*public float calDocHbt(Doctor doctor){
         float rcm = calDocRCM(doctor);
         float rep = calDocREP(doctor);
         if(rcm<0||rcm>1||rep<0||rep>1){
@@ -138,6 +149,6 @@ public class TrustCalculator {
             return -1;
         }
         return rcm<rep?rcm:rep;     //HBT = min(RCM,REP)
-    }
+    }*/
 
 }
