b795f1c9ba92aa9584f7c7dc35dc6139fdd2b521
xdCao <705083979@qq.com>
2017-12-25 14:21:08 +0800

    12-25 commit -2

--- src/main/resources/sql/medicine.sql
+++ src/main/resources/sql/medicine.sql
@@ -10,12 +10,9 @@ Target Server Type    : MYSQL
 Target Server Version : 50624
 File Encoding         : 65001
 
-Date: 2017-12-24 17:43:27
+Date: 2017-12-25 14:20:28
 */
 
-SET character_set_client =utf8;
-SET character_set_results = utf8;
-SET character_set_connection = utf8;
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
@@ -54,7 +51,7 @@ CREATE TABLE `doctor` (
   `account` varchar(30) NOT NULL,
   `isIn` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of doctor
@@ -67,11 +64,6 @@ INSERT INTO `doctor` VALUES ('5', '曹医生', '4', '西电老科', '13336758765
 INSERT INTO `doctor` VALUES ('6', 'caohao', '1', 'xian', '13337865544', '2017-12-20', '1', '1', '1', '1', '1', '123', '444', '0');
 INSERT INTO `doctor` VALUES ('7', '12', '4', '12', '12', '2017-12-21', '2', '0', '4', '2', '1', '12', '21', '0');
 INSERT INTO `doctor` VALUES ('8', '12', '4', '12', '12', '2017-12-21', '2', '0', '4', '2', '1', '12', '244', '0');
-INSERT INTO `doctor` VALUES ('9', 'Alice', '1', '西安电子科技大学', '13476859976', '2017-12-05', '3', '5', '3', '1', '0', '111', '123', '0');
-INSERT INTO `doctor` VALUES ('10', 'Bob', '2', '太白南路', '18747657765', '2017-12-18', '3', '3', '2', '3', '0', '111', '233', '0');
-INSERT INTO `doctor` VALUES ('11', 'Cindy', '3', '科技路', '13390874532', '2017-11-13', '3', '2', '1', '3', '0', '111', '333', '0');
-INSERT INTO `doctor` VALUES ('12', 'Danny', '4', '吉祥路', '14378650098', '2017-01-17', '4', '5', '4', '4', '0', '111', '433', '0');
-
 
 -- ----------------------------
 -- Table structure for others
@@ -120,14 +112,14 @@ CREATE TABLE `patient` (
   KEY `fk_doctor` (`doctor_id`),
   CONSTRAINT `fk_doctor` FOREIGN KEY (`doctor_id`) REFERENCES `doctor` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_trust_attr` FOREIGN KEY (`trustAttr_id`) REFERENCES `trust_attr` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of patient
 -- ----------------------------
 INSERT INTO `patient` VALUES ('11', '张三', '2', '13338926199', '2017-12-23', '14', '0', '没有特殊病例', '111', '111', '37.5', '100', '80', '0');
 INSERT INTO `patient` VALUES ('13', '赵四', '2', '17765478998', '2017-12-23', '15', '0', 'nothing', '111', '112', '0', '0', '0', '0');
-INSERT INTO `patient` VALUES ('10', '测试MT的病人', '9', '17765478996', '2017-12-20', '13', '0', 'nothing', '111', 'hhh','0', '0', '0', '0');
+INSERT INTO `patient` VALUES ('14', '王五', '5', '17765478998', '2017-12-25', '16', '0', 'nothing', '111', '113', '0', '0', '0', '0');
 
 -- ----------------------------
 -- Table structure for sys_log
@@ -162,14 +154,14 @@ CREATE TABLE `trust_attr` (
   `demand_workage` tinyint(4) NOT NULL,
   `demand_degree` tinyint(4) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of trust_attr
 -- ----------------------------
 INSERT INTO `trust_attr` VALUES ('14', '4', '3', '2', '1');
 INSERT INTO `trust_attr` VALUES ('15', '1', '2', '3', '3');
-INSERT INTO `trust_attr` VALUES ('13', '3', '2', '3', '3');
+INSERT INTO `trust_attr` VALUES ('16', '1', '2', '3', '3');
 
 -- ----------------------------
 -- Table structure for user_log
@@ -182,12 +174,14 @@ CREATE TABLE `user_log` (
   `evaluate_value` tinyint(4) NOT NULL,
   `evaluate_content` varchar(255) DEFAULT NULL,
   `evaluate_date` date NOT NULL,
+  `user_type` int(1) NOT NULL,
+  `user_name` varchar(30) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user_log
 -- ----------------------------
-INSERT INTO `user_log` VALUES ('14', '1', '张医生', '90', '态度好', '2017-12-06');
-INSERT INTO `user_log` VALUES ('15', '2', '李医生', '80', '医德高尚', '2017-12-19');
-INSERT INTO `user_log` VALUES ('16', '3', '王医生', '50', 'aaaa', '2017-11-14');
+INSERT INTO `user_log` VALUES ('14', '1', '张医生', '90', '态度好', '2017-12-06', '1', '张三');
+INSERT INTO `user_log` VALUES ('15', '2', '李医生', '80', '医德高尚', '2017-12-19', '1', '赵四');
+INSERT INTO `user_log` VALUES ('16', '3', '王医生', '50', 'aaaa', '2017-11-14', '1', '王五');
